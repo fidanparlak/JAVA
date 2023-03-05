4b3e5bd1f31d34b0c7fbb943b2fa85778fad286b
Fidan <parlakfidan1981@gmail.com>
2023-03-05 23:45:33 +0100

    Fidan branchi 2.degisiklik

--- src/day01_programlamayaGiris/Ozellik1.java
+++ src/day01_programlamayaGiris/Ozellik1.java
@@ -3,5 +3,9 @@ package day01_programlamayaGiris;
 public class Ozellik1 {
     public static void main(String[] args) {
         System.out.println("Yeni calismalarim");
+        System.out.println("2.degisiklik");
+
+        System.out.println("ortak classde degisiklik");
+
     }
 }
